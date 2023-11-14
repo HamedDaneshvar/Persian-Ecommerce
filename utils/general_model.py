e5ba448a32a8ab4ef08fca8606f56eff4eeacede
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-14 07:50:55 +0330

    docs(main): Add docstring for general model and replace tabs with spaces

--- utils/general_model.py
+++ utils/general_model.py
@@ -3,12 +3,26 @@ from django.utils.translation import gettext as _
 
 
 class GeneralModel(models.Model):
-	create_at = models.DateTimeField(
-		auto_now_add=True,
-		verbose_name=_('Created Time'),)
-	updated_at = models.DateTimeField(
-		auto_now=True,
-		verbose_name=_('Updated Time'),)
+    """
+    An abstract base model class that provides common fields for other models.
 
-	class Meta:
-		abstract = True
+    Fields:
+    - create_at: A DateTimeField representing the creation time of the
+      model instance.
+    - updated_at: A DateTimeField representing the last update time of
+      the model instance.
+
+    Meta:
+    - abstract: Specifies that this model is an abstract base class and
+      should not be used to create database tables.
+
+    """
+    create_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created Time'),)
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated Time'),)
+
+    class Meta:
+        abstract = True
