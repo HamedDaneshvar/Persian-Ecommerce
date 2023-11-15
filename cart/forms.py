5d0c62e01d01d7abee71e245142b5af8427fa12b
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-15 18:30:00 +0330

    style(cart): convert all tabs with 4 spaces and add necessary space in code & convert relative import to absolute import

--- cart/forms.py
+++ cart/forms.py
@@ -2,16 +2,18 @@ from django import forms
 
 
 class CartAddProductForm(forms.Form):
-	quantity = forms.IntegerField(initial=1,
-								  min_value=1,
-								  label="",
-								  widget=forms.widgets.NumberInput(
-										attrs={
-											"class": "form-control cart-quantity-input",
-											"step": "1",
-										}
-								  ),
-	)
-	override = forms.BooleanField(required=False,
-								  initial=False,
-								  widget=forms.HiddenInput)
+    quantity = forms.IntegerField(
+        initial=1,
+        min_value=1,
+        label="",
+        widget=forms.widgets.NumberInput(
+            attrs={
+                "class": "form-control cart-quantity-input",
+                "step": "1", }
+        ),
+    )
+    override = forms.BooleanField(
+        required=False,
+        initial=False,
+        widget=forms.HiddenInput
+    )
