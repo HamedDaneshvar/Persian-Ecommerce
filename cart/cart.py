631854fb0790261458b2ddd45969b11844cdeaf8
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-15 18:45:49 +0330

    docs(cart): Added some docstring and comment to cart file to describe what it does

--- cart/cart.py
+++ cart/cart.py
@@ -5,24 +5,33 @@ from coupons.models import Coupon
 
 
 class Cart():
+    """
+    Represents a shopping cart for a user session.
+    """
 
     def __init__(self, request):
         """
-        initialize the cart for every session(user).
-        if cart is exist set cart and if not, create one.
+        Initializes the cart for every session (user).
+        If the cart exists, it sets the cart; otherwise, it creates a new one.
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
+            # Save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
-        # store current applied coupon
+        # Store current applied coupon
         self.coupon_id = self.session.get("coupon_id")
 
     def add(self, product, quantity=1, override_quantity=False):
         """
-        Add a product to the cart or update its quantity.
+        Adds a product to the cart or updates its quantity.
+
+        Parameters:
+        - product: The product to be added or updated.
+        - quantity: The quantity of the product to be added.
+        - override_quantity: A boolean indicating whether to override the
+          existing quantity.
         """
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -35,18 +44,24 @@ class Cart():
         self.save()
 
     def save(self):
-        # mark the session as "modified" to make sure it gets saved
+        """
+        Marks the session as "modified" to ensure it gets saved.
+        """
         self.session.modified = True
 
     def remove(self, product):
         """
-        Remove a product from the cart.
+        Removes a product from the cart.
+
+        Parameter:
+        - product: The product to be removed from the cart.
         """
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
 
+        # Remove coupon from session if cart is empty
         if len(self) == 0:
             try:
                 if self.session.get("coupon_id", None):
@@ -57,11 +72,11 @@ class Cart():
 
     def __iter__(self):
         """
-        Iterate over the items in the cart and get the products
-        from the database.
+        Iterates over the items in the cart and gets the corresponding
+        products from the database.
         """
         product_ids = self.cart.keys()
-        # get the product objects and add them to the cart
+        # Get the product objects and add them to the cart
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
@@ -73,11 +88,14 @@ class Cart():
 
     def __len__(self):
         """
-        Count all items in the cart.
+        Returns the total number of items in the cart.
         """
         return sum(item["quantity"] for item in self.cart.values())
 
     def get_total_price(self):
+        """
+        Returns the total price of all items in the cart.
+        """
         return sum(Decimal(item["price"]) * item["quantity"]
                    for item in self.cart.values())
 
@@ -93,6 +111,9 @@ class Cart():
 
     @property
     def coupon(self):
+        """
+        Returns the applied coupon if exists; otherwise, returns None.
+        """
         if self.coupon_id:
             try:
                 return Coupon.objects.get(id=self.coupon_id)
@@ -101,10 +122,16 @@ class Cart():
         return None
 
     def get_discount(self):
+        """
+        Returns the discount amount based on the applied coupon.
+        """
         if self.coupon:
             return (self.coupon.discount / Decimal(100)) \
                     * self.get_total_price()
         return Decimal(0)
 
     def get_total_price_after_discount(self):
+        """
+        Returns the total price after applying the discount.
+        """
         return self.get_total_price() - self.get_discount()
