587453828763092abcbeb21dd273a85c8653a076
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-15 19:35:20 +0330

    docs(cart): Added some docstring to forms file

--- cart/forms.py
+++ cart/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 
 class CartAddProductForm(forms.Form):
+    """
+    Form used for adding a product to the cart.
+
+    This form provides fields for specifying the quantity of the product to
+    be added, as well as an optional field to override the quantity if needed.
+    """
     quantity = forms.IntegerField(
         initial=1,
         min_value=1,
