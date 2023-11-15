28a9cc2e6d54add94e54937403c5a5ff305b78b2
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-15 13:27:46 +0330

    docs(shop): Added some docstring to admin file to describe what it does

--- shop/admin.py
+++ shop/admin.py
@@ -7,14 +7,25 @@ from .models import (
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin class for managing Category model.
+    """
+
     list_display = ["name", "slug"]
+    # Automatically populate the slug field based on the name
     prepopulated_fields = {"slug": ("name",), }
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin class for managing Product model.
+    """
+
     list_display = ["name", "slug", "price", "available",
                     "create_at", "updated_at",]
     list_filter = ["available", "create_at", "updated_at",]
+    # Editable fields directly in the admin list view
     list_editable = ["price", "available",]
+    # Automatically populate the slug field based on the name
     prepopulated_fields = {"slug": ("name",), }
