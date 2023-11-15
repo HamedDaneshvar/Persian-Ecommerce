9446f6e67649dd523c660e10bf46f3479d8eb0c3
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-15 19:49:42 +0330

    docs(cart): Added some docstring to context processors file

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -2,4 +2,12 @@ from cart.cart import Cart
 
 
 def cart(request):
+    """
+    Context processor that adds the cart object to the request context.
+
+    This context processor retrieves the cart for the current user from the
+    session and adds it to the request context, making it accessible in
+    templates as 'cart'. The cart object provides convenient methods to manage
+    the products in the cart.
+    """
     return {'cart': Cart(request)}
