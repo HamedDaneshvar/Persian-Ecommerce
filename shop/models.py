951677f4e093799e6e203b298930701e7dabfaf5
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-15 07:25:56 +0330

    style(shop): convert all tabs with 4 spaces and add necessary space in code

--- shop/models.py
+++ shop/models.py
@@ -5,68 +5,67 @@ from utils.general_model import GeneralModel
 
 
 class Category(GeneralModel):
-	name = models.CharField(
-		max_length=250,
-		verbose_name=_("Name"),)
-	slug = models.SlugField(
-		max_length=250,
-		unique=True,
-		verbose_name=_("Slug"),)
+    name = models.CharField(
+        max_length=250,
+        verbose_name=_("Name"),)
+    slug = models.SlugField(
+        max_length=250,
+        unique=True,
+        verbose_name=_("Slug"),)
 
-	class Meta:
-		ordering = ["name",]
-		indexes = [
-			models.Index(fields=['name']),
-		]
-		verbose_name = _("Category")
-		verbose_name_plural = _("Categories")
+    class Meta:
+        ordering = ["name",]
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def get_absolute_url(self):
-		return reverse('shop:product_list_by_category', args=[self.slug,])
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug,])
 
 
 class Product(GeneralModel):
-	category = models.ForeignKey(
-		Category,
-		on_delete=models.CASCADE,
-		related_name="products",
-		verbose_name=_("Category"))
-	name = models.CharField(
-		max_length=250,
-		verbose_name=_("Name"),)
-	slug = models.SlugField(
-		max_length=250,
-		unique=True,
-		verbose_name=_("Slug"),)
-	image = models.ImageField(
-		upload_to="products/%Y/%m/%d",
-		blank=True,
-		verbose_name=_("Image"),)
-	description = models.TextField(
-		blank=True,
-		verbose_name=_("Description"),)
-	price = models.DecimalField(
-		max_digits=12,
-		decimal_places=2,
-		verbose_name=_("Price"),)
-	available = models.BooleanField(
-		default=True,
-		verbose_name=_("Available"),)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+        verbose_name=_("Category"))
+    name = models.CharField(
+        max_length=250,
+        verbose_name=_("Name"),)
+    slug = models.SlugField(
+        max_length=250,
+        unique=True,
+        verbose_name=_("Slug"),)
+    image = models.ImageField(
+        upload_to="products/%Y/%m/%d",
+        blank=True,
+        verbose_name=_("Image"),)
+    description = models.TextField(
+        blank=True,
+        verbose_name=_("Description"),)
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        verbose_name=_("Price"),)
+    available = models.BooleanField(
+        default=True,
+        verbose_name=_("Available"),)
 
-	class Meta:
-		ordering = ["name"]
-		indexes = [
-			models.Index(fields=["id", "slug",]),
-			models.Index(fields=["name",]),
-			models.Index(fields=["-create_at",]),
-		]
-	
-	def __str__(self):
-		return self.name
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["id", "slug",]),
+            models.Index(fields=["name",]),
+            models.Index(fields=["-create_at",]),
+        ]
 
-	def get_absolute_url(self):
-		return reverse('shop:product_detail', args=[self.id, self.slug,])
-		
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug,])
--- shop/views.py
+++ shop/views.py
@@ -1,72 +1,74 @@
 from django.shortcuts import (
-	render,
-	get_object_or_404,
+    render,
+    get_object_or_404,
 )
 from cart.forms import CartAddProductForm
 from .models import (
-	Category,
-	Product,
+    Category,
+    Product,
 )
 
+
 def product_list(request, category_slug=None):
-	category = None
-	grid = False
-	product_exist = True
-	products_description = ""
-	categories = Category.objects.all()
-	products = Product.objects.filter(available=True,)
-	if len(products) == 0:
-		product_exist = False
-		products_description = "این فروشگاه محصولی را ثبت نکرده است."
+    category = None
+    grid = False
+    product_exist = True
+    products_description = ""
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True,)
+    if len(products) == 0:
+        product_exist = False
+        products_description = "این فروشگاه محصولی را ثبت نکرده است."
 
-	if category_slug:
-		category = get_object_or_404(
-			Category,
-			slug=category_slug,
-		)
-		products = products.filter(category=category)
-		if len(products) == 0:
-			product_exist = False
-			products_description = "برای این دسته‌بندی محصولی ثبت نشده است."
+    if category_slug:
+        category = get_object_or_404(
+            Category,
+            slug=category_slug,
+        )
+        products = products.filter(category=category)
+        if len(products) == 0:
+            product_exist = False
+            products_description = "برای این دسته‌بندی محصولی ثبت نشده است."
 
-	# show in grid or list type
-	if request.GET.get('style', None) == 'list':
-		grid = False
-	else:
-		grid = True
+    # show in grid or list type
+    if request.GET.get('style', None) == 'list':
+        grid = False
+    else:
+        grid = True
 
-	cart_product_form = CartAddProductForm(auto_id=False, 
-										   initial={"override": True,})
-	
-	return render(
-		request=request,
-		template_name='shop/product/list.html',
-		context={
-			'category': category,
-			'categories': categories,
-			'products': products,
-			'product_exist': product_exist,
-			'products_description': products_description,
-			'grid': grid,
-			'cart_product_form': cart_product_form,
-		}
-	)
+    cart_product_form = CartAddProductForm(auto_id=False,
+                                           initial={"override": True, })
+
+    return render(
+        request=request,
+        template_name='shop/product/list.html',
+        context={
+            'category': category,
+            'categories': categories,
+            'products': products,
+            'product_exist': product_exist,
+            'products_description': products_description,
+            'grid': grid,
+            'cart_product_form': cart_product_form,
+        }
+    )
+
 
 def product_detail(request, id, slug):
-	product = get_object_or_404(
-		Product,
-		id=id,
-		slug=slug,
-		available=True,
-	)
+    product = get_object_or_404(
+        Product,
+        id=id,
+        slug=slug,
+        available=True,
+    )
 
-	cart_product_form = CartAddProductForm()
+    cart_product_form = CartAddProductForm()
 
-	return render(
-		request=request,
-		template_name='shop/product/detail.html',
-		context={
-			'product': product,
-			'cart_product_form': cart_product_form,
-		}
-	)
+    return render(
+        request=request,
+        template_name='shop/product/detail.html',
+        context={
+            'product': product,
+            'cart_product_form': cart_product_form,
+        }
+    )
