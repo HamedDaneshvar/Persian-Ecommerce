bf6e5a85fdfbaf6cc9c9aa5d13e77472e14090c8
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-17 18:59:11 +0330

    style(coupons): convert all tabs with 4 spaces and add necessary space in code

--- coupons/models.py
+++ coupons/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 from django.utils.translation import gettext as _
 from django.core.validators import (
-	MinValueValidator,
-	MaxValueValidator,
+    MinValueValidator,
+    MaxValueValidator,
 )
 from utils.general_model import GeneralModel
 
 
 class Coupon(GeneralModel):
-	code = models.CharField(max_length=50,
-							unique=True,
-							verbose_name=_("Code"),)
-	valid_from = models.DateTimeField(verbose_name=_("Valid from"))
-	valid_to = 	models.DateTimeField(verbose_name=_("Valid to"))
-	discount = models.IntegerField(
-		validators=[MinValueValidator(0), MaxValueValidator(100)],
-		help_text=_("Percentage value (0 to 100)"))
-	active = models.BooleanField(verbose_name=_("Active"))
+    code = models.CharField(max_length=50,
+                            unique=True,
+                            verbose_name=_("Code"),)
+    valid_from = models.DateTimeField(verbose_name=_("Valid from"))
+    valid_to = models.DateTimeField(verbose_name=_("Valid to"))
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        help_text=_("Percentage value (0 to 100)"))
+    active = models.BooleanField(verbose_name=_("Active"))
 
-	def __str__(self):
-		return self.code
+    def __str__(self):
+        return self.code
--- coupons/views.py
+++ coupons/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import (
-	render,
-	redirect,
+    redirect,
 )
 from django.utils import timezone
-from .models import Coupon
-from .forms import CouponApplyForm
+from coupons.models import Coupon
+from coupons.forms import CouponApplyForm
 
 
 def coupon_apply(request):
-	now = timezone.now()
-	if request.method == "POST":
-		form = CouponApplyForm(request.POST)
-		if form.is_valid():
-			code = form.cleaned_data["code"]
-			try:
-				coupon = Coupon.objects.get(code__iexact=code,
-											valid_from__lte=now,
-											valid_to__gte=now,
-											active=True,)
-				request.session["coupon_id"] = coupon.id
-				request.session["coupon_code"] = code
-			except Coupon.DoesNotExist:
-				request.session["coupon_id"] = None
-				request.session["coupon_code"] = code
-	else:
-		request.session["coupon_id"] = None
-		del request.session["coupon_code"]
+    now = timezone.now()
+    if request.method == "POST":
+        form = CouponApplyForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data["code"]
+            try:
+                coupon = Coupon.objects.get(code__iexact=code,
+                                            valid_from__lte=now,
+                                            valid_to__gte=now,
+                                            active=True,)
+                request.session["coupon_id"] = coupon.id
+                request.session["coupon_code"] = code
+            except Coupon.DoesNotExist:
+                request.session["coupon_id"] = None
+                request.session["coupon_code"] = code
+    else:
+        request.session["coupon_id"] = None
+        del request.session["coupon_code"]
 
-	
-	return redirect("cart:cart_detail")
+    return redirect("cart:cart_detail")
