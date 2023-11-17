03e92f87652ec0bd226acb6002993cd736d4f517
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-17 19:15:42 +0330

    docs(coupons): Added some docstring to admin file to describe what it is

--- coupons/models.py
+++ coupons/models.py
@@ -8,6 +8,21 @@ from utils.general_model import GeneralModel
 
 
 class Coupon(GeneralModel):
+    """
+    Represents a coupon that can be applied to a purchase in the e-commerce
+    system.
+
+    Attributes:
+    - code (str): The unique code for the coupon.
+    - valid_from (datetime): The date and time from which the coupon is valid.
+    - valid_to (datetime): The date and time until which the coupon is valid.
+    - discount (int): The percentage value of the discount (0 to 100).
+    - active (bool): Indicates whether the coupon is currently active.
+
+    Methods:
+    - __str__: Returns a string representation of the coupon (the code).
+
+    """
     code = models.CharField(max_length=50,
                             unique=True,
                             verbose_name=_("Code"),)
