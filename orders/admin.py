cc80712d8ec51115e11fff80a2b3965e1597c1e5
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-18 09:34:37 +0330

    docs(orders): add some docstring for admin file and describe what it is

--- orders/admin.py
+++ orders/admin.py
@@ -6,13 +6,38 @@ from orders.models import (
 
 
 class OrderItemInline(admin.TabularInline):
+    """
+    Inline admin class for OrderItem model.
+
+    This inline class allows the administration of OrderItem objects
+    within the Order admin page in a tabular format.
+
+    Attributes:
+        - model (OrderItem): The model associated with the inline.
+        - raw_id_fields (list): The fields displayed as raw ID fields in the
+          admin interface.
+    """
     model = OrderItem
     raw_id_fields = ["product"]
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["full_name", "email", "phone", "address", 
+    """
+    Admin class for Order model.
+
+    This class customizes the administration interface for the Order model.
+    It defines the list display, list filters, and inline classes.
+
+    Attributes:
+        - list_display (list): The fields displayed in the list view of the
+          admin interface.
+        - list_filter (list): The fields used for filtering in the admin
+          interface.
+        - inlines (list): The inline classes associated with the Order admin
+          page.
+    """
+    list_display = ["full_name", "email", "phone", "address",
                     "transport", "get_total_cost", "fee",
                     "paid", "create_at", "updated_at"]
     list_filter = ["paid", "create_at", "updated_at",]
