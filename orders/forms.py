bec97bcb3f514a74aad5b53f4b631aaa81dd3e91
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-18 10:54:15 +0330

    docs(orders): add some docstring for form file

--- orders/forms.py
+++ orders/forms.py
@@ -5,5 +5,15 @@ from orders.models import Order
 class OrderCreateForm(forms.ModelForm):
 
     class Meta:
+        """
+        Form class for creating Order objects.
+
+        This form is used to create new Order objects based on the provided
+        fields.
+
+        Attributes:
+            - model (Order): The model associated with the form.
+            - fields (list): The fields to include in the form.
+        """
         model = Order
         fields = ["full_name", "email", "phone", "address",]
