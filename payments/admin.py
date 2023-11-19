3f8d215906d500b9e5b074e5e8f19a3667391624
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-19 11:46:11 +0330

    docs(payments): add some docstring for admin file

--- payments/admin.py
+++ payments/admin.py
@@ -4,6 +4,15 @@ from payments.models import Payment
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Payment model.
+
+    Attributes:
+        - list_display (list): The list of fields to display in the admin list
+          view.
+        - list_filter (list): The list of fields to use for filtering in the
+          admin list view.
+    """
     list_display = ["name", "types", "available",
                     "create_at", "updated_at",]
     list_filter = ["available", "create_at", "updated_at",]
