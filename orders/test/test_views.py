058392a791d658b984a523178bfbfc48609acb13
Hamed Daneshvar <hmddaneshvar@gmail.com>
2023-11-25 14:20:01 +0330

    test(orders): add some test for send_to_payment function view

--- orders/test/test_views.py
+++ orders/test/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.sessions.middleware import SessionMiddleware
 from shop.models import Category, Product
@@ -9,7 +10,7 @@ from transportation.models import Transport
 from coupons.models import Coupon
 from cart.cart import Cart
 from orders.models import OrderItem, Order
-from orders.views import order_create
+from orders.views import order_create, send_to_payment
 
 
 class OrderCreateViewTest(TestCase):
@@ -307,3 +308,100 @@ class OrderDetailViewTest(TestCase):
         self.assertEqual(response.context['order'], self.order)
         self.assertEqual(len(response.context['items']), 1)
         self.assertTemplateUsed(response, 'orders/order/detail.html')
+
+
+class SendToPaymentViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.session_middleware = SessionMiddleware()
+        self.user = self.create_user()
+        self.category = self.create_category()
+        self.product = self.create_product("Product", 10)
+        self.transport = self.create_transport()
+        self.order = self.create_order()
+
+    def create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email='test@example.com',
+            password='testpass',
+            full_name='John Doe'
+        )
+        return user
+
+    def create_category(self):
+        category = Category.objects.create(
+            name='Test Category',
+            slug='test-category'
+        )
+        return category
+
+    def create_product(self, name, price):
+        image = SimpleUploadedFile(
+            name='test_image.jpg', content=b'',
+            content_type='image/jpeg')
+        product = Product.objects.create(
+            name=f'{name}',
+            category=self.category,
+            slug=f'{name}',
+            description='Test description',
+            price=price,
+            image=image,
+            available=True
+        )
+        return product
+
+    def create_transport(self):
+        transport = Transport.objects.create(
+            name="Express Shipping",
+            delivery="Fast delivery within 2-3 business days",
+            price=10.99,
+            activate=True
+        )
+        return transport
+
+    def create_order(self):
+        order = Order.objects.create(
+            user=self.user,
+            full_name="John Doe",
+            email="test@example.com",
+            phone="1234567890",
+            address="Test Address",
+            transport=self.transport)
+        OrderItem.objects.create(
+            order=order,
+            product=self.product,
+            price=10,
+            quantity=1)
+        return order
+
+    def test_send_to_payment_authenticated_user(self):
+        request = self.factory.get(reverse('orders:order_payment',
+                                           args=[self.order.id]))
+        request.user = self.user
+        self.session_middleware.process_request(request)
+        request.session.save()
+        response = send_to_payment(request, self.order.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('payments:request'))
+        self.assertEqual(request.session['amount'],
+                         f'{float(self.order.get_total_cost()):.2f}')
+        self.assertEqual(request.session['order_id'], self.order.id)
+
+    def test_send_to_payment_order_not_found(self):
+        request = self.factory.get(reverse('orders:order_payment', args=[999]))
+        request.user = self.user
+        response = send_to_payment(request, 999)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('orders:orders_list'))
+
+    def test_send_to_payment_anonymous_user(self):
+        request = self.factory.get(reverse('orders:order_payment',
+                                           args=[self.order.id]))
+        request.user = AnonymousUser()
+        self.session_middleware.process_request(request)
+        request.session.save()
+        response = send_to_payment(request, self.order.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('accounts:login') + "?next=" +
+                         reverse("orders:order_payment", args=[self.order.id]))
