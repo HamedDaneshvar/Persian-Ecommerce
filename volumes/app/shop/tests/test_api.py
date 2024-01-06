691c7db4bcbc90345aed62763eaaface447ade62
Hamed Daneshvar <hmddaneshvar@gmail.com>
2024-01-06 16:21:11 +0330

    fix(shop): fix conflict

--- volumes/app/shop/tests/test_api.py
+++ volumes/app/shop/tests/test_api.py
@@ -13,7 +13,6 @@ User = get_user_model()
 class CategoryAPITestCase(APITestCase):
     def setUp(self):
         self.user = self.create_superuser()
-<<<<<<< HEAD
         self.category = Category.objects.create(name='Test Category',
                                                 slug='test-category')
         self.url = reverse('api-shop:category-detail',
@@ -22,13 +21,6 @@ class CategoryAPITestCase(APITestCase):
         request = factory.get(self.url)
         self.serializer_data = CategorySerializer(
             self.category, context={'request': request}).data
-=======
-        self.category = Category.objects.create(name='Test Category', slug='test-category')
-        self.url = reverse('api-shop:category-detail', kwargs={'pk': self.category.pk})
-        factory = RequestFactory()
-        request = factory.get(self.url)
-        self.serializer_data = CategorySerializer(self.category, context={'request': request}).data
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
 
     def create_superuser(self):
         user = User.objects.create_superuser(
@@ -59,17 +51,12 @@ class CategoryAPITestCase(APITestCase):
 
     def test_create_category_unauthorized(self):
         data = {'name': 'New Category', 'slug': 'new-category'}
-<<<<<<< HEAD
         response = self.client.post(reverse('api-shop:category-list-create'),
                                     data)
-=======
-        response = self.client.post(reverse('api-shop:category-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertFalse(Category.objects.filter(name='New Category').exists())
 
     def test_create_category_authorized(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -77,12 +64,6 @@ class CategoryAPITestCase(APITestCase):
         data = {'name': 'New Category', 'slug': 'new-category'}
         response = self.client.post(reverse('api-shop:category-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': 'New Category', 'slug': 'new-category'}
-        response = self.client.post(reverse('api-shop:category-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Category.objects.filter(name='New Category').exists())
 
@@ -90,7 +71,6 @@ class CategoryAPITestCase(APITestCase):
 class ProductAPITestCase(APITestCase):
     def setUp(self):
         self.user = self.create_superuser()
-<<<<<<< HEAD
         self.category = Category.objects.create(name='Test Category',
                                                 slug='test-category')
         self.product = Product.objects.create(name='Test Product',
@@ -103,14 +83,6 @@ class ProductAPITestCase(APITestCase):
         request = factory.get(self.url)
         self.serializer_data = ProductSerializer(
             self.product, context={'request': request}).data
-=======
-        self.category = Category.objects.create(name='Test Category', slug='test-category')
-        self.product = Product.objects.create(name='Test Product', slug='test-product', price=10.99, category=self.category)
-        self.url = reverse('api-shop:product-detail', kwargs={'pk': self.product.pk})
-        factory = RequestFactory()
-        request = factory.get(self.url)
-        self.serializer_data = ProductSerializer(self.product, context={'request': request}).data
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
 
     def create_superuser(self):
         user = User.objects.create_superuser(
@@ -127,14 +99,10 @@ class ProductAPITestCase(APITestCase):
 
     def test_update_product(self):
         self.client.force_login(self.user)
-<<<<<<< HEAD
         new_data = {'name': 'Updated Product',
                     'slug': 'updated-product',
                     'price': '19.99',
                     'category_id': self.category.pk}
-=======
-        new_data = {'name': 'Updated Product', 'slug':'updated-product', 'price': '19.99', 'category_id': self.category.pk}
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         response = self.client.put(self.url, new_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], new_data['name'])
@@ -148,20 +116,14 @@ class ProductAPITestCase(APITestCase):
         self.assertFalse(Product.objects.filter(pk=self.product.pk).exists())
 
     def test_create_product_unauthorized(self):
-<<<<<<< HEAD
         data = {'name': 'New Product', 'price': 9.99,
                 'category_id': self.category.pk}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        data = {'name': 'New Product', 'price': 9.99, 'category_id': self.category.pk}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertFalse(Product.objects.filter(name='New Product').exists())
 
     def test_create_product_authorized(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -170,19 +132,12 @@ class ProductAPITestCase(APITestCase):
                 'category_id': self.category.pk}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': 'New Product', 'slug': 'new-product', 'price': 9.99, 'category_id': self.category.pk}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Product.objects.filter(name='New Product').exists())
 
 
 class ProductListAPITestCase(APITestCase):
     def setUp(self):
-<<<<<<< HEAD
         self.category = Category.objects.create(name='Test Category',
                                                 slug='test-category')
         Product.objects.create(name='Product 1', slug='product-1',
@@ -191,16 +146,10 @@ class ProductListAPITestCase(APITestCase):
         Product.objects.create(name='Product 2', slug='product-2',
                                price=19.99, category=self.category,
                                available=False)
-=======
-        self.category = Category.objects.create(name='Test Category', slug='test-category')
-        Product.objects.create(name='Product 1', slug='product-1', price=10.99, category=self.category, available=True)
-        Product.objects.create(name='Product 2', slug='product-2', price=19.99, category=self.category, available=False)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
 
     def test_list_products_unauthorized(self):
         response = self.client.get(reverse('api-shop:product-list-create'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-<<<<<<< HEAD
         # Only one available product should be returned.
         self.assertEqual(len(response.data), 1)
 
@@ -219,25 +168,10 @@ class ProductListAPITestCase(APITestCase):
                 'category_id': self.category.pk}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        self.assertEqual(len(response.data), 1)  # Only one available product should be returned.
-
-    def test_list_products_authorized(self):
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        response = self.client.get(reverse('api-shop:product-list-create'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)  # Both products should be returned.
-
-    def test_create_product_unauthorized(self):
-        data = {'name': 'New Product', 'price': 9.99, 'category_id': self.category.pk}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertFalse(Product.objects.filter(name='New Product').exists())
 
     def test_create_product_authorized(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -246,17 +180,10 @@ class ProductListAPITestCase(APITestCase):
                 'category_id': self.category.pk}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': 'New Product', 'slug': 'new-product', 'price': 9.99, 'category_id': self.category.pk}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Product.objects.filter(name='New Product').exists())
 
     def test_create_product_missing_category(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -264,17 +191,10 @@ class ProductListAPITestCase(APITestCase):
         data = {'name': 'New Product', 'price': 9.99}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': 'New Product', 'price': 9.99}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(Product.objects.filter(name='New Product').exists())
 
     def test_create_product_invalid_category(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -283,17 +203,10 @@ class ProductListAPITestCase(APITestCase):
                 'category_id': 999}  # Non-existent category ID
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': 'New Product', 'slug': 'new-product', 'price': 9.99, 'category_id': 999}  # Non-existent category ID
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(Product.objects.filter(name='New Product').exists())
 
     def test_create_product_invalid_price(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -302,17 +215,10 @@ class ProductListAPITestCase(APITestCase):
                 'price': 'invalid', 'category_id': self.category.pk}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': 'New Product', 'slug': 'new-product', 'price': 'invalid', 'category_id': self.category.pk}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(Product.objects.filter(name='New Product').exists())
 
     def test_create_product_invalid_name(self):
-<<<<<<< HEAD
         user = User.objects.create_superuser(username='admin@example.com',
                                              email='admin@example.com',
                                              password='admin123')
@@ -321,11 +227,5 @@ class ProductListAPITestCase(APITestCase):
                 'category_id': self.category.pk}
         response = self.client.post(reverse('api-shop:product-list-create'),
                                     data)
-=======
-        user = User.objects.create_superuser(username='admin@example.com', email='admin@example.com',  password='admin123')
-        self.client.force_authenticate(user=user)
-        data = {'name': '', 'slug': 'new-product', 'price': 9.99, 'category_id': self.category.pk}
-        response = self.client.post(reverse('api-shop:product-list-create'), data)
->>>>>>> 6f148bd5c28ac6b554188f29e5165117dc5df2ab
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(Product.objects.filter(name='').exists())
